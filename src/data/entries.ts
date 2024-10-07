3e65e99dd3a21117ed4e1102afdcf6d7d3cf6020
denkiwakame <denkivvakame@gmail.com>
2024-10-07 17:47:03 +0900

    add entries

---
+++ src/data/entries.ts
@@ -0,0 +1,17 @@
+export type Entry = {
+  title: string;
+  techs: string[];
+  link: string;
+  isComingSoon?: boolean;
+};
+
+const entries: Entry[] = [
+  {
+    title:
+      "Essential Pointers for Smooth NVIDIA Driver, CUDA, and Docker Integration",
+    techs: ["CUDA", "docker", "driver"],
+    link: "https://denkiwakame.notion.site/Mastering-NVIDIA-Setup-Essential-Pointers-for-Smooth-CUDA-Docker-Integration-64a44a98e7f940c483b6600c927f41f8",
+  },
+];
+
+export default entries;
