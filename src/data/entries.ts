9422406814eb10940a1d95d5261b1ea25cc1f720
denkiwakame <denkivvakame@gmail.com>
2024-12-02 17:21:01 +0900

    add entry

--- src/data/entries.ts
+++ src/data/entries.ts
@@ -12,6 +12,11 @@ const entries: Entry[] = [
     techs: ["CUDA", "docker", "driver"],
     link: "https://denkiwakame.notion.site/Mastering-NVIDIA-Setup-Essential-Pointers-for-Smooth-CUDA-Docker-Integration-64a44a98e7f940c483b6600c927f41f8",
   },
+  {
+    title: "Setting up a faiss build environment with Pixi",
+    techs: ["CUDA", "Pixi", "faiss", "conda", "PyPI"],
+    link: "https://denkiwakame.notion.site/pixi-faiss-14c3175c6b6a80b39eebf2ab8eb7b05d",
+  },
 ];
 
 export default entries;
